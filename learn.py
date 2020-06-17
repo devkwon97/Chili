ffa96e05f789f077834a34c8436e29386e02d497
devkwon97 <kkm97721@gmail.com>
2020-06-17 23:58:23 +0900

    commit

---
+++ learn.py
@@ -0,0 +1,45 @@
+﻿import numpy
+import tensorflow
+from pandas.io.parsers import read_csv
+import io, sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
+
+model = tensorflow.global_variables_initializer();
+
+data = read_csv('amount.csv', sep=',')
+
+xy = numpy.array(data, dtype=numpy.float32)
+
+x_data = xy[:, 1:-1]
+y_data = xy[:, [-1]]
+
+X = tensorflow.placeholder(tensorflow.float32, shape=[None, 4])
+Y = tensorflow.placeholder(tensorflow.float32, shape=[None, 1])
+
+W = tensorflow.Variable(tensorflow.random_normal([4, 1]), name="weight")
+b = tensorflow.Variable(tensorflow.random_normal([1]), name="bias")
+
+# 가설설정
+hypothesis = tensorflow.matmul(X, W) + b
+
+# 비용 함수 정의
+cost = tensorflow.reduce_mean(tensorflow.square(hypothesis - Y))
+
+# 옵티마이저로 내려가는 경사값 알고리즘구현
+optimizer = tensorflow.train.GradientDescentOptimizer(learning_rate=0.000005)
+train = optimizer.minimize(cost)
+
+sess = tensorflow.Session()
+
+sess.run(tensorflow.global_variables_initializer())
+
+# 학습실행
+for step in range(100001):
+    cost1, hypo_, _ = sess.run([cost, hypothesis, train], feed_dict={X: x_data, Y: y_data})
+    if step % 500 == 0:
+        print("#", step, " 손실 비용: ", cost1)
+        print("- 고추 가격: ", hypo_[0][0], "원")
+
+saver = tensorflow.train.Saver()
+save_path = saver.save(sess, "./saved.cpkt")
+print('학습 완료.')
---
+++ prediction.py
@@ -0,0 +1,34 @@
+﻿import numpy
+import tensorflow
+
+# 텐서 홀더 설정
+X = tensorflow.placeholder(tensorflow.float32, shape=[None, 4])
+Y = tensorflow.placeholder(tensorflow.float32, shape=[None, 1])
+
+W = tensorflow.Variable(tensorflow.random_normal([4, 1]), name="weight")
+b = tensorflow.Variable(tensorflow.random_normal([1]), name="bias")
+
+# 가설을 정의
+hypothesis = tensorflow.matmul(X, W) + b
+
+# 학습한 모델 불러오기
+saver = tensorflow.train.Saver()
+model = tensorflow.global_variables_initializer()
+
+# 4가지 변인 입력
+avg_temp = float(input('평균 온도: '))
+min_temp = float(input('최저 온도: '))
+max_temp = float(input('최고 온도: '))
+rain_fall = float(input('강수량: '))
+
+with tensorflow.Session() as sess:
+    sess.run(model)
+    save_path = "./saved.cpkt"
+    saver.restore(sess, save_path)
+
+    data = ((avg_temp, min_temp, max_temp, rain_fall), (0, 0, 0, 0))
+    arr = numpy.array(data, dtype=numpy.float32)
+
+    x_data = arr[0:4]
+    dict = sess.run(hypothesis, feed_dict={X: x_data})
+    print('고추의 예측 가격은',dict[0][0],'원')
